53147aca58e70ecf5dd661404ede5b6380bed258
shin5ok <shin5ok@55mp.com>
2023-04-29 16:50:22 +0900

    show some log

--- pubsub.go
+++ pubsub.go
@@ -11,21 +11,26 @@ import (
 
 func publishLog(client *pubsub.Client, topicName string, data map[string]interface{}, async bool) error {
 
-	jsonData, _ := json.Marshal(data)
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
 
 	message := &pubsub.Message{
 		Data: jsonData,
 	}
 
 	topic := client.Topic(topicName)
+	ctx := context.Background()
 	if async {
 		go func() {
-			res := topic.Publish(context.Background(), message)
-			log.Printf("async %+v\n", res)
+			res := topic.Publish(ctx, message)
+			log.Printf("Async %+v\n", res)
 		}()
 	} else {
-		res := topic.Publish(context.Background(), message)
-		log.Printf("sync %+v\n", res)
+		res := topic.Publish(ctx, message)
+		log.Printf("Sync %+v\n", res)
 	}
 
 	return nil
